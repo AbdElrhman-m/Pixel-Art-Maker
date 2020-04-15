67f5096b545ea162dafdc8bf2d5e64ab076c9bca
AbdElrhman Mohamed <superbido185@gmail.com>
2020-04-15 18:12:41 +0200

    docs: add comments

--- designs.js
+++ designs.js
@@ -1,14 +1,12 @@
 // define all the dom element need for this project
-const 
-      sizePicker    = document.querySelector("#sizePicker"),
-      // the submit button to make a grid
-      createGridBn  = document.querySelector("input[type=submit]"),
+const sizePicker    = document.querySelector("#sizePicker"),
+      createGridBn  = document.querySelector("input[type=submit]"), // the submit button to make a grid
       pixelCanvas   = document.querySelector("#pixelCanvas");
 
 let colorPicker   = document.querySelector("#colorPicker").value,
     inputHeight   = document.querySelector("#inputHeight").value,
     inputWidth    = document.querySelector("#inputWidth").value;
-// test
+
 
 function cleanGrid(){
     "use strict";
@@ -25,13 +23,14 @@ function makeGrid(inputHeight, inputWidth, colorPicker, pixelCanvas) {
     // reassign the varriable everytime
     inputHeight   = document.querySelector("#inputHeight").value;
     inputWidth    = document.querySelector("#inputWidth").value;
-    
+    // make the grade
     for (let i = 1; i <= inputHeight; i++) {
         let gRow = document.createElement('tr');
         pixelCanvas.appendChild(gRow);
         for (let j = 1; j <= inputWidth; j++) {
             let gCell = document.createElement('td');
             gRow.appendChild(gCell);
+            //assign event for each cell
             gCell.addEventListener('mousedown', function() {
                 colorPicker   = document.querySelector("#colorPicker").value;
                 this.style.backgroundColor = colorPicker;
